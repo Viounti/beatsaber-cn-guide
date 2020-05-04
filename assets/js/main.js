4b4f63e785a060bc2108b3484413caf8cbbaa3fe
WGzeyu <785777793@qq.com>
2020-05-04 15:07:15 +0800

    更换模板

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,122 +1,51 @@
-(function(){
-	// FAQ Template - by CodyHouse.co
-  var FaqTemplate = function(element) {
-		this.element = element;
-		this.sections = this.element.getElementsByClassName('cd-faq__group');
-		this.triggers = this.element.getElementsByClassName('cd-faq__trigger');
-		this.faqContainer = this.element.getElementsByClassName('cd-faq__items')[0];
-		this.faqsCategoriesContainer = this.element.getElementsByClassName('cd-faq__categories')[0];
-		this.faqsCategories = this.faqsCategoriesContainer.getElementsByClassName('cd-faq__category');
-  	this.faqOverlay = this.element.getElementsByClassName('cd-faq__overlay')[0];
-  	this.faqClose = this.element.getElementsByClassName('cd-faq__close-panel')[0];
-  	this.scrolling = false;
-  	initFaqEvents(this);
-  };
+jQuery(document).ready(function($){
+	var introSection = $('#cd-intro-background'),
+		introSectionHeight = introSection.height(),
+		//change scaleSpeed if you want to change the speed of the scale effect
+		scaleSpeed = 0.3,
+		//change opacitySpeed if you want to change the speed of opacity reduction effect
+		opacitySpeed = 1; 
+	
+	//update this value if you change this breakpoint in the style.css file (or _layout.scss if you use SASS)
+	var MQ = 1170;
 
-  function initFaqEvents(faqs) {
-  	// click on a faq category
-		faqs.faqsCategoriesContainer.addEventListener('click', function(event){
-			var category = event.target.closest('.cd-faq__category');
-			if(!category) return;
-			var mq = getMq(faqs),
-				selectedCategory = category.getAttribute('href').replace('#', '');
-			if(mq == 'mobile') { // on mobile, open faq panel
-				event.preventDefault();
-				faqs.faqContainer.scrollTop = 0;
-				Util.addClass(faqs.faqContainer, 'cd-faq__items--slide-in');
-				Util.addClass(faqs.faqClose, 'cd-faq__close-panel--move-left');
-				Util.addClass(faqs.faqOverlay, 'cd-faq__overlay--is-visible');
-				var selectedSection = faqs.faqContainer.getElementsByClassName('cd-faq__group--selected');
-				if(selectedSection.length > 0) {
-					Util.removeClass(selectedSection[0], 'cd-faq__group--selected');
-				}
-				Util.addClass(document.getElementById(selectedCategory), 'cd-faq__group--selected');
-			} else { // on desktop, scroll to section
-				if(!window.requestAnimationFrame) return;
-				event.preventDefault();
-				var windowScrollTop = window.scrollY || document.documentElement.scrollTop;
-				Util.scrollTo(document.getElementById(selectedCategory).getBoundingClientRect().top + windowScrollTop + 2, 200);
-			}
-		});
+	triggerAnimation();
+	$(window).on('resize', function(){
+		triggerAnimation();
+	});
 
-		// on mobile -> close faq panel
-		faqs.faqOverlay.addEventListener('click', function(event){
-			closeFaqPanel(faqs);
-		});
-		faqs.faqClose.addEventListener('click', function(event){
-			event.preventDefault();
-			closeFaqPanel(faqs);
-		});
-
-		// on desktop -> toggle faq content visibility when clicking on the trigger element
-		faqs.faqContainer.addEventListener('click', function(event){
-			if(getMq(faqs) != 'desktop') return;
-			var trigger = event.target.closest('.cd-faq__trigger');
-			if(!trigger) return;
-			event.preventDefault();
-			var content = trigger.nextElementSibling,
-				parent = trigger.closest('li'),
-				bool = Util.hasClass(parent, 'cd-faq__item-visible');
-
-			Util.toggleClass(parent, 'cd-faq__item-visible', !bool);
-
-			//store initial and final height - animate faq content height
-			Util.addClass(content, 'cd-faq__content--visible');
-			var initHeight = bool ? content.offsetHeight: 0,
-				finalHeight = bool ? 0 : content.offsetHeight;
-			
-			if(window.requestAnimationFrame) {
-				Util.setHeight(initHeight, finalHeight, content, 200, function(){
-					heighAnimationCb(content, bool);
-				});
-			} else {
-				heighAnimationCb(content, bool);
-			}
-		});
-		
-		if(window.requestAnimationFrame) {
-			// on scroll -> update selected category
-			window.addEventListener('scroll', function(){
-				if(getMq(faqs) != 'desktop' || faqs.scrolling) return;
-				faqs.scrolling = true;
-				window.requestAnimationFrame(updateCategory.bind(faqs)); 
+	//bind the scale event to window scroll if window width > $MQ (unbind it otherwise)
+	function triggerAnimation(){
+		if($(window).width()>= MQ) {
+			$(window).on('scroll', function(){
+				//The window.requestAnimationFrame() method tells the browser that you wish to perform an animation- the browser can optimize it so animations will be smoother
+				window.requestAnimationFrame(animateIntro);
+			});
+		} else {
+			$(window).off('scroll');
+		}
+	}
+	//assign a scale transformation to the introSection element and reduce its opacity
+	function animateIntro () {
+		var scrollPercentage = ($(window).scrollTop()/introSectionHeight).toFixed(5),
+			scaleValue = 1 - scrollPercentage*scaleSpeed;
+		//check if the introSection is still visible
+		if( $(window).scrollTop() < introSectionHeight) {
+			introSection.css({
+			    '-moz-transform': 'scale(' + scaleValue + ') translateZ(0)',
+			    '-webkit-transform': 'scale(' + scaleValue + ') translateZ(0)',
+				'-ms-transform': 'scale(' + scaleValue + ') translateZ(0)',
+				'-o-transform': 'scale(' + scaleValue + ') translateZ(0)',
+				'transform': 'scale(' + scaleValue + ') translateZ(0)',
+				'opacity': 1 - scrollPercentage*opacitySpeed
 			});
 		}
-  };
+	}
 
-  function closeFaqPanel(faqs) {
-  	Util.removeClass(faqs.faqContainer, 'cd-faq__items--slide-in');
-  	Util.removeClass(faqs.faqClose, 'cd-faq__close-panel--move-left');
-  	Util.removeClass(faqs.faqOverlay, 'cd-faq__overlay--is-visible');
-  };
-
-  function getMq(faqs) {
-		//get MQ value ('desktop' or 'mobile') 
-		return window.getComputedStyle(faqs.element, '::before').getPropertyValue('content').replace(/'|"/g, "");
-  };
-
-  function updateCategory() { // update selected category -> show green rectangle to the left of the category
-  	var selected = false;
-		for(var i = 0; i < this.sections.length; i++) {
-			var top = this.sections[i].getBoundingClientRect().top,
-				bool = (top <= 0) && (-1*top < this.sections[i].offsetHeight);
-			Util.toggleClass(this.faqsCategories[i], 'cd-faq__category-selected', bool);
-			if(bool) selected = true;
-		}
-		if(!selected) Util.addClass(this.faqsCategories[0], 'cd-faq__category-selected');
-		this.scrolling = false;
-  };
-
-  function heighAnimationCb(content, bool) {
-		content.removeAttribute("style");
-		if(bool) Util.removeClass(content, 'cd-faq__content--visible');
-  };
-
-  var faqTemplate = document.getElementsByClassName('js-cd-faq'),
-  	faqArray = [];
-  if(faqTemplate.length > 0) {
-		for(var i = 0; i < faqTemplate.length; i++) {
-			faqArray.push(new FaqTemplate(faqTemplate[i])); 
-		}
-  };
-})();
+	/********************************
+		open/close submenu on mobile
+	********************************/
+	$('.cd-main-nav').on('click', function(event){
+		if($(event.target).is('.cd-main-nav')) $(this).children('ul').toggleClass('is-visible');
+	});
+});
